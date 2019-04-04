cfce850a7e465bd1c2b879e7d8c1203af8cd8f49
Nicholas <nicholasrolstad@outlook.com>
2019-04-04 11:40:52 -0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -3,10 +3,10 @@ import requests
 #This tool will attempt to download a series of geojson files from a layers REST endpoint
 
 # parameters
-name = ''                       # name of county, etc. for file name
-main_url = ''                   # REST endpoint for layer, will look like 'http://gis.co.ym.mn.gov/arcgis/rest/services/YellowMedicine/YellowMedicine_DataLayers/MapServer/40'
-min_value =  1                  # integer, lowest value of objectid
-max_value =  999                # integer, highest value of objectid (or rounded up to nearest 1000-1 e.g. round 6758 to 6999 NOT 7000)
+name = 'chippewa'                       # name of county, etc. for file name
+main_url = 'https://www.co.chippewa.mn.us/arcgis/rest/services/Assessor/TaxParcels/FeatureServer/0'                   # REST endpoint for layer, will look like 'http://gis.co.ym.mn.gov/arcgis/rest/services/YellowMedicine/YellowMedicine_DataLayers/MapServer/40'
+min_value =  2984943                  # integer, lowest value of objectid
+max_value =  2996999                # integer, highest value of objectid (or rounded up to nearest 1000-1 e.g. round 6758 to 6999 NOT 7000)
 object_id_name = 'OBJECTID'     # may be something like 'OBJECTID_1'
 
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)     Chrome/37.0.2049.0 Safari/537.36'}
